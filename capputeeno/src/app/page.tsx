92bda16d5ee9bed9d5eadaa43fb46192b8b1de70
johnmelodev <joaohenrique1231500@gmail.com>
2024-07-09 08:51:15 +0100

    finished filter

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,28 +1,10 @@
-import Image from "next/image";
+import { FilterBar } from "@/components/filter-bar";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-        </p>
-
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-      </div>
+      <FilterBar />
     </main>
   );
 }
---
+++ capputeeno/src/components/filter-by-type.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { useFilter } from "@/hooks/useFilter"
+import { FilterType } from "@/types/filter-types"
+import styled from "styled-components"
+
+interface FilterItemProps {
+    selected: boolean
+}
+
+const FilterList = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+`
+
+const FilterItem = styled.li<FilterItemProps>`
+    font-family: inherit;
+    font-weight: ${props => props.selected ? '600' : '400'};
+    font-size: 16px;
+    line-height: 22px;
+    text-align: center;
+    text-transform: uppercase;
+    cursor: pointer;
+
+    color: var(--text-dark);
+
+    border-bottom: ${props => props.selected ? '4px solid var(--orange-low)' : ''};
+`
+
+export function FilterByType() {
+    const { type, setType } = useFilter();
+
+    const handleChangeType = (value: FilterType) => {
+        setType(value)
+    }
+
+    return (
+        <FilterList>
+            <FilterItem
+                selected={type === FilterType.ALL}
+                onClick={() => handleChangeType(FilterType.ALL)}
+            >
+                All the products
+            </FilterItem>
+            <FilterItem
+                selected={type === FilterType.SHIRTS}
+                onClick={() => handleChangeType(FilterType.SHIRTS)}
+            >
+                Shirts
+            </FilterItem>
+            <FilterItem
+                selected={type === FilterType.MUGS}
+                onClick={() => handleChangeType(FilterType.MUGS)}
+            >
+                Mugs
+            </FilterItem>
+        </FilterList >
+    )
+}
